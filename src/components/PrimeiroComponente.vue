c42acb069d21a434679cf0689009be7649bfb610
mesaque <mesaque1212@gmail.com>
2022-04-08 19:57:53 -0300

    Adicionando template, estudando componentes

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>Olá vue</h1>
+    <h1>Olá Javascript</h1>
+</template>
+
+<script>
+export default {
+    name: 'PrimeiroComponente',
+    name: 'PrimeiroComponente'
+}
+</script>
