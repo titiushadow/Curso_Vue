af2f8ca6e09210f2d6058aaeac668b548ffabac5
mesaque <mesaque1212@gmail.com>
2022-04-08 23:30:26 -0300

    Importanto

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
   <primeiro-componente/>
   <p>A linguagem que eu uso é: {{ Testando }}</p>
   <life-cycle />
+  <pessoa />
 </div>
 </template>
 
 <script>
 import PrimeiroComponente from './components/PrimeiroComponente.vue'
 import LifeCycle from './components/LifeCycle.vue'
+import Pessoa from './components/Pessoa.vue'
 
     export default{
       name:'app',
       components: {
         PrimeiroComponente,
-        LifeCycle
+        LifeCycle,
+        Pessoa
       },
       data() {
         return {
