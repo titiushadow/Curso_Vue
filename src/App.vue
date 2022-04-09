5545ce5d2dbe5ab3bfb5750b6d81b63334677d38
mesaque <mesaque1212@gmail.com>
2022-04-08 21:36:48 -0300

    importando LifeCycle de componentes

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,19 @@
 <div>
   <primeiro-componente/>
   <p>A linguagem que eu uso é: {{ Testando }}</p>
+  <life-cycle />
 </div>
 </template>
 
 <script>
 import PrimeiroComponente from './components/PrimeiroComponente.vue'
+import LifeCycle from './components/LifeCycle.vue'
+
     export default{
       name:'app',
       components: {
-        PrimeiroComponente 
+        PrimeiroComponente,
+        LifeCycle
       },
       data() {
         return {
