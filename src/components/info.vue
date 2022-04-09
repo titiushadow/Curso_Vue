bc0334f0ab0d0535b88c379065a28050e715aa60
mesaque <mesaque1212@gmail.com>
2022-04-08 23:54:34 -0300

    Diretivas, (v-if, v-else, v-show).

--- src/components/info.vue
+++ src/components/info.vue
@@ -1,5 +1,7 @@
 <template>
-  <p>Estou trabalhando no momento.</p>
+<!-- v-if and v-else -->
+  <p v-if="esta_codando">Estou codando no momento.</p>
+  <p v-else>Estou em busca de novas oportunidades</p>
   <p>Utilizo as seguintes tecnologias:</p>
   <ul>
     <li>PHP/Laravel</li>
@@ -7,13 +9,18 @@
     <li>PHP</li>
     <li>Python</li>
   </ul>
+
+  <!-- v-show -->
+  <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
 </template>
 <script>
 export default {
   name: 'Info',
   data() {
     return { 
-      
+      esta_codando: false,
+      mostrar_email: true,
+      email: 'mesaque1212@gmail.com'
     }
   }
 }
