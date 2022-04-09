ff9368ba8e942b96401a33f8bb83739792c582a7
mesaque <mesaque1212@gmail.com>
2022-04-09 10:06:01 -0300

    Criação do arquvio Header, usando css scoped

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Projetos</a></li>
+    <li><a href="#">Contato</a></li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: 'Header'
+  }
+</script>
+
+<style scoped>
+  ul {
+    list-style: none;
+    display: flex;
+    background-color: #999;
+  }
+  li {
+    margin-right: 10px;
+  }
+  a {
+    color: #DDD;
+    text-decoration: none;
+    transition: .5s;
+  }
+  a:hover {
+    color: red;
+  }
+</style>
