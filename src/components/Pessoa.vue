2dd95f361dd15b48836909bb9aa8399c66c25cfb
mesaque <mesaque1212@gmail.com>
2022-04-08 23:28:27 -0300

    Criando o componente Pessoa

---
+++ src/components/Pessoa.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <h2>Esta é a descrição da pessoa: {{ name }}</h2>
+  <info />
+  <Form />
+</div>
+</template>
+
+<script>
+  import info from './info.vue'
+  import Form from './Form.vue'
+
+export default {
+  name: 'Pessoa',
+  components: {
+    info,
+    Form
+  },
+  data() {
+    return {
+      name: 'Mesaque'
+    }
+  },
+}
+</script>
