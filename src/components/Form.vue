881e42a3bf80a3a4c728712d80cfdaea2067a95e
mesaque <mesaque1212@gmail.com>
2022-04-08 23:30:13 -0300

    Criando o componente Form para reaproveitamento de codigo

---
+++ src/components/Form.vue
@@ -0,0 +1,28 @@
+<template>
+<h2>Meu primeiro form com VUE</h2>
+    <div>
+        <input-text />
+    </div>
+
+     <div>
+        <input-text />
+    </div>
+
+     <div>
+        <submit />
+    </div>
+</template>
+
+<script>
+import InputText from './Form/InputText.vue'
+import Submit from './Form/Submit.vue'
+export default {
+    name: 'Form',
+    components: {
+        InputText,
+        Submit
+    }
+
+}
+</script>
+   
