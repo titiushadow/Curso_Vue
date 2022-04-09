194993ec14af0ad49364317e7dee85965b5b43ec
mesaque <mesaque1212@gmail.com>
2022-04-08 23:28:56 -0300

    Criando o componente info

---
+++ src/components/info.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>Estou trabalhando no momento.</p>
+  <p>Utilizo as seguintes tecnologias:</p>
+  <ul>
+    <li>PHP/Laravel</li>
+    <li>JavaScript</li>
+    <li>PHP</li>
+    <li>Python</li>
+  </ul>
+</template>
+<script>
+export default {
+  name: 'Info',
+  data() {
+    return { 
+      
+    }
+  }
+}
+</script>
