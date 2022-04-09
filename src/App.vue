2f2f90d842ff26d9fe44d97c7bfe50ac035c832d
mesaque <mesaque1212@gmail.com>
2022-04-09 10:05:24 -0300

    importando o Header.vue para app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
 <div>
+  <Header />
   <primeiro-componente/>
-  <p>A linguagem que eu uso é: {{ Testando }}</p>
+  <p class="paragrafo-pai">A linguagem que eu uso é: {{ Testando }}</p>
   <life-cycle />
   <pessoa />
 </div>
 </template>
 
 <script>
+import Header from './components/Header.vue' 
 import PrimeiroComponente from './components/PrimeiroComponente.vue'
 import LifeCycle from './components/LifeCycle.vue'
 import Pessoa from './components/Pessoa.vue'
@@ -15,6 +17,7 @@ import Pessoa from './components/Pessoa.vue'
     export default{
       name:'app',
       components: {
+        Header,
         PrimeiroComponente,
         LifeCycle,
         Pessoa
@@ -26,3 +29,18 @@ import Pessoa from './components/Pessoa.vue'
       },
     }
 </script>
+
+<style>
+  body {
+    background-color: #333;
+    color: #fff;
+  }
+
+  a {
+    color:#ff0000;
+  }
+
+  .teste {
+    background-color: #000;
+  }
+</style>
