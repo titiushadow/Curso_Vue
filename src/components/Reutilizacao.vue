6f7207f641c2803a355c78c932157288a2a4ae46
mesaque <mesaque1212@gmail.com>
2022-04-09 15:30:55 -0300

    Criação do arquivo para reutilização

---
+++ src/components/Reutilizacao.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <button @click="ativar">Cliquei aqui!</button>
+        <span v-show="ativou">Ativado!</span>
+    </div>
+</template>
+
+<script>
+export default {
+    nome: 'Reutilizacao',
+    data() {
+        return {
+            ativou: false           
+        }
+    },
+    
+    // Metodo para ativar o botão 
+    methods: {
+        ativar() {
+            this.ativou = !this.ativou
+        }
+    },
+}
+</script>
