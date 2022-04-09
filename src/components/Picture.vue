83c8f7f1a6750489272edeec5fcf144bb045ff2a
mesaque <mesaque1212@gmail.com>
2022-04-09 15:32:12 -0300

    Adicionando  componente em Picture.vue

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,9 +1,15 @@
 <template>
   <img :src="avatar" :alt="descricao">
+  <Reutilizacao />
 </template>
 <script>
+
+import Reutilizacao from './Reutilizacao.vue'
 export default {
   name: 'Picture',
+  components: {
+    Reutilizacao
+  },
   data() {
     return { 
       avatar: "/img/avatar.png",
