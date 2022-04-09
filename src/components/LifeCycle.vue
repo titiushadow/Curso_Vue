66d9de59a24bac88cca5cc4e4a51fa3639350424
mesaque <mesaque1212@gmail.com>
2022-04-08 21:37:13 -0300

    Criando o arquivo LifeCycleHooks

---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>My name is: {{ nome }}</h1>    
+</template>
+
+<script>
+export default {
+    name:'LifeCycle',
+    data() {
+        return {
+            nome: 'Mesaque'
+        }
+    },
+    created(){
+        setTimeout(() => {
+            this.nome = 'Meseikers'
+        }, 1000);  
+    },
+    mounted() {
+        setTimeout(() => {
+            this.nome = 'João'
+        }, 2000);
+    },
+}
+</script>
