75441672f40f383ae02f3d0b46cdccfc72626b4e
mesaque <mesaque1212@gmail.com>
2022-04-09 00:20:07 -0300

    Colocando link com v-bind e imagem

--- src/components/info.vue
+++ src/components/info.vue
@@ -12,15 +12,28 @@
 
   <!-- v-show -->
   <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+
+  <p>Para acessar meu portifólio
+    <a v-bind:href="meu_link">
+      basta clicar aqui!
+    </a>
+  </p>
+  <picture />
+
 </template>
 <script>
+
+import Picture from './Picture'
 export default {
   name: 'Info',
+  components: {
+    Picture
+  },
   data() {
     return { 
-      esta_codando: false,
-      mostrar_email: true,
-      email: 'mesaque1212@gmail.com'
+      esta_trabalhando: false,
+      mostrar_email: false,
+      url_portfolio: "https://github.com/titiushadow"
     }
   }
 }
