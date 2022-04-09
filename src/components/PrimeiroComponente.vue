7c149b2052609fb21b32cdd5d209fce010b29eba
mesaque <mesaque1212@gmail.com>
2022-04-09 14:51:30 -0300

    Adicionando os components de MultiplosEventos

--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -3,11 +3,17 @@
         <h1>Olá vue</h1>
         <p>Meu nome é: {{ nome }}, E meu trabalho é: {{ Profissão }}</p>
     </div>
+    <MultiplosEventos />
 </template>
 
 <script>
+
+import MultiplosEventos from './MultiplosEventos.vue'
 export default {
     name: 'PrimeiroComponente',
+    components: {
+        MultiplosEventos       
+    },
     data() {
         return {
             nome: "Mesaque",
